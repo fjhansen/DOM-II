298c11dfc77016158bca77edbda8d362049d59a1
Frances Hansen <francesjuniperhansen@gmail.com>
2020-02-08 19:12:20 -0800

    added mouse events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// Mouse Events
+
+const imgContent = document.querySelector(".img-content");
+
+imgContent.addEventListener("mouseenter", () => {
+    imgContent.style.transform = "scale(1.2)";
+    imgContent.style.transition = "transform 0.3s"
+})
+
+imgContent.addEventListener("mouseleave", () => {
+    imgContent.style.transform = "scale(1)";
+})
